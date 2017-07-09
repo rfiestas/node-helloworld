4da141557b7c2336901f207c225dceaaa1a13a43
rfiestas <rfiestas@gmail.com>
2017-07-09 17:46:10 +0200

    Custom nodejs

--- puppet/modules/custom_nodejs/manifests/init.pp
+++ puppet/modules/custom_nodejs/manifests/init.pp
@@ -1,11 +1,19 @@
-class nodejs_app {
-	file { 'nodejs_app.conf':
-	  path    => '/etc/init/nodejs_app.conf',
-	  ensure  => file,
-	  notify  => [ User['www-data'], Service['nodejs_app'] ]
-	  content => template('nodejs_app.conf'),
-	}
-	service { 'nodejs_app':
-	  ensure => running,
-	}
+class custom_nodejs {
+
+  file { 'nodejs_app.conf':
+    path    => '/etc/init/nodejs_app.conf',
+    ensure  => file,
+    notify  => [ Exec['start_nodejs_app'] ],
+    content => template('custom_nodejs/nodejs_app.conf.erb'),
+	require => [ User['www-data'] ]
+  }
+  
+  exec { 'start_nodejs_app':
+    command => 'start nodejs_app',
+    path    => '/sbin/:/bin/',
+  }
+  
+  $npm_packages = hiera('nodejs::npm')
+  create_resources(nodejs::npm, $npm_packages)
+
 }
