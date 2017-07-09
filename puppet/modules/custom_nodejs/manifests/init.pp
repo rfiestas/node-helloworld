3e6c2ab09c0f0af2541e0ec83311c11145c8e65f
rfiestas <rfiestas@gmail.com>
2017-07-09 18:28:14 +0200

    Change require

--- puppet/modules/custom_nodejs/manifests/init.pp
+++ puppet/modules/custom_nodejs/manifests/init.pp
@@ -5,12 +5,13 @@ class custom_nodejs {
     ensure  => file,
     notify  => [ Exec['start_nodejs_app'] ],
     content => template('custom_nodejs/nodejs_app.conf.erb'),
-	require => [ User['www-data'] ]
+	require => [ User['www-data'], Class['nodejs'] ]
   }
   
   exec { 'start_nodejs_app':
     command => 'start nodejs_app',
     path    => '/sbin/:/bin/',
+	unless  => 'status nodejs_app'
   }
   
   $npm_packages = hiera('nodejs::npm')
