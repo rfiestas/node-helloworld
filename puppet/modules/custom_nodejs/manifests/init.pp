98b60ead688484f1cfb17d0d4290c5fec2e217c6
rfiestas <rfiestas@gmail.com>
2017-07-09 16:50:01 +0200

    Rename nodejs_app service

---
+++ puppet/modules/custom_nodejs/manifests/init.pp
@@ -0,0 +1,11 @@
+class nodejs_app {
+	file { 'nodejs_app.conf':
+	  path    => '/etc/init/nodejs_app.conf',
+	  ensure  => file,
+	  notify  => [ User['www-data'], Service['nodejs_app'] ]
+	  content => template('nodejs_app.conf'),
+	}
+	service { 'nodejs_app':
+	  ensure => running,
+	}
+}
--- puppet/modules/custom_nodejs/manifests/nodejs_app.pp
+++
@@ -1,9 +0,0 @@
-file { 'nodejs_app.conf':
-  path    => '/etc/init/nodejs_app.conf',
-  ensure  => file,
-  notify  => [ User['www-data'], Service['nodejs_app'] ]
-  content => template('nodejs_app.conf'),
-}
-service { 'nodejs_app':
-  ensure => running,
-}
